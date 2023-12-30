2d789dd2fa0722ba48a92965b96058d7c5a055ba
s4hlo <rfl.magno02@gmail.com>
2023-12-30 15:28:08 -0300

    feat(easy): :pencil2: 9.ts problem

---
+++ 9.palindrome-number.ts
@@ -0,0 +1,20 @@
+/*
+ * @lc app=leetcode id=9 lang=typescript
+ *
+ * [9] Palindrome Number
+ */
+
+// @lc code=start
+function isPalindrome(x: number): boolean {
+  if (x < 0) return false;
+  if (x < 10) return true;
+  const str = x.toString();
+  const len = str.length;
+  for (let i = 0; i < len / 2; i++) {
+    if (str[i] !== str[len - 1 - i]) return false;
+  }
+  return true;
+    
+};
+// @lc code=end
+
